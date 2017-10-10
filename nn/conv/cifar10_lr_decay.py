b37f7d98a2b1b424ca7c4ecf80dc0b06398f78f5
Michael Schwab <mcstar@gmail.com>
2017-10-10 06:58:57 -0500

    cifar10 with lr decay rate

--- nn/conv/cifar10_lr_decay.py
+++ nn/conv/cifar10_lr_decay.py
@@ -41,3 +41,30 @@ testY = lb.transform(testY)
 # initialize the label names for the CIFAR-10 dataset
 labelNames = ["airplane", "automobile", "bird", "cat", "deer",
     "dog", "frog", "horse", "ship", "truck"]
+
+callbacks = [LearningRateScheduler(step_decay)]
+#initialize the optimizer
+opt = SGD(lr=0.01, momentum=0.9, nesterov=True)
+model = MiniVGGNet.build(width=32, height=32, depth=3, classes=10)
+model.compile(loss="categorical_crossentropy", optimizer=opt, metric=["accuracy"])
+H = model.fit(trainX, trainY, validation_data=(testX, testY), 
+    batch_size=64, epochs=40, callbacks=callbacks, verbose=1)
+
+# evaluate the network
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=64)
+print(classification_report(testY.argmax(axis=1),
+predictions.argmax(axis=1), target_names=labelNames))
+
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, 40), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 40), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 40), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, 40), H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy on CIFAR-10")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.savefig(args["output"])
